b41bc96a6c1ea86bb42cb0b90214ab272857d7e7
Lukyanov Artem <protrollcat@gmail.com>
2025-05-08 16:03:47 +0300

    news client upd

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import {getUsersLeadersList} from "_api/admin/userAcceptList"
 import ClientPage from "./page.client"
+import {getNewsList} from "_api/admin/newsList"
 
 export default async function Page() {
 	const leaders = await getUsersLeadersList()
-	return <ClientPage leaders={leaders} />
+	const newsList = await getNewsList()
+	return <ClientPage leaders={leaders} newsList={newsList} />
 }
